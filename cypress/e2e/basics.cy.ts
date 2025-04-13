df8a0f672eaa0d8e4018e5b4530003cbf47f35f0
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-13 18:13:31 +0300

    Find text and img

--- cypress/e2e/basics.cy.ts
+++ cypress/e2e/basics.cy.ts
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:4200')
-  })
+describe('tasks page', () => {
+  it('should render the main image', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('.main-header img');
+  });
+  it('should display the page title', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('.main-header h1').should('have.text', "Angular Tasks");
+    cy.get('.main-header h1').contains("Angular Tasks");
+    cy.contains('Angular Tasks');
+
+  });
 })
