0e8c2356cf791cb8f3709ec993f726ff066945f3
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-13 18:31:00 +0300

    Elements length

--- cypress/e2e/basics.cy.ts
+++ cypress/e2e/basics.cy.ts
@@ -5,6 +5,7 @@ describe('tasks page', () => {
   });
   it('should display the page title', () => {
     cy.visit('http://localhost:4200');
+    cy.get('h1').should('have.length', 1);
     cy.get('.main-header h1').should('have.text', "Angular Tasks");
     cy.get('.main-header h1').contains("Angular Tasks");
     cy.contains('Angular Tasks');
