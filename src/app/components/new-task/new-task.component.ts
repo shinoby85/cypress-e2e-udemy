14124c292fff7f64990e74efbd67cf65b45dc17e
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-13 12:35:45 +0300

    Add new-task template

--- src/app/components/new-task/new-task.component.ts
+++ src/app/components/new-task/new-task.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, output} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  addTask = output<{title: string; summary: string; category: string;}>();
+  cancel = output();
 
+  form = new FormGroup({
+    title: new FormControl("", {
+      validators: [Validators.required]
+    }),
+    summary: new FormControl("", {validators: [Validators.required]}),
+    category: new FormControl("moderate"),
+  });
+
+  submitHandler(event: Event) {
+    event.preventDefault();
+
+    const enteredTitle = this.form.controls.title.value || '';
+    const enteredSummary = this.form.controls.summary.value || '';
+    const chosenCategory = this.form.controls.category.value || '';
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    const taskData = {
+      title: enteredTitle,
+      summary: enteredSummary,
+      category: chosenCategory,
+    };
+    this.addTask.emit(taskData);
+  }
 }
