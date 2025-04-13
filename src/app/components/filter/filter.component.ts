72f89c1f35002e1e81d629daf937ce844384d51c
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-13 12:03:25 +0300

    Add filter template

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, output, Output} from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  filter = output<string>();
 
+  filterChangeHandler(event: Event) {
+    this.filter.emit((event.target as HTMLSelectElement).value);
+  }
 }
