00eddd6d1e752d0413ff5b86bc1721ba6e5e7f60
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-13 18:57:28 +0300

    Use find

--- cypress/e2e/basics.cy.ts
+++ cypress/e2e/basics.cy.ts
@@ -2,6 +2,8 @@ describe('tasks page', () => {
   it('should render the main image', () => {
     cy.visit('http://localhost:4200');
     cy.get('.main-header img');
+    cy.get('.main-header').find('img');
+
   });
   it('should display the page title', () => {
     cy.visit('http://localhost:4200');
