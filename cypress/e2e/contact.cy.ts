46bcb37ff1b70e973a9faec0985da24929e3b32d
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-17 10:13:40 +0300

    Use alias

--- cypress/e2e/contact.cy.ts
+++ cypress/e2e/contact.cy.ts
@@ -1,14 +1,21 @@
 describe('contact form', () => {
   it('should submit the form', () => {
+    // const submitBtn = '[data-cy="contact-btn-submit"]';
     cy.visit('http://localhost:4200/about');
     cy.get('[data-cy="contact-input-message"').type('Hello World');
     cy.get('[data-cy="contact-input-name"]').type('John Doe');
     cy.get('[data-cy="contact-input-email"]').type('test@example.com');
-    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
-    cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
-    cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
-    cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+    // cy.get(submitBtn).contains('Send Message');
+    // cy.get(submitBtn).should('not.have.attr', 'disabled');
+    // cy.get(submitBtn).click();
+    // cy.get(submitBtn).contains('Sending...');
+    // cy.get(submitBtn).should('have.attr', 'disabled');
+    cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+    cy.get('@submitBtn').contains('Send Message');
+    cy.get('@submitBtn').should('not.have.attr', 'disabled');
+    cy.get('@submitBtn').click();
+    cy.get('@submitBtn').contains('Sending...');
+    cy.get('@submitBtn').should('have.attr', 'disabled');
 
   })
 })
