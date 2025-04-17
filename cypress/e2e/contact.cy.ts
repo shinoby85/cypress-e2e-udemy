a33bb81f4f8122fe0298919ea5b944d09089efe8
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-17 08:35:42 +0300

    Test form

---
+++ cypress/e2e/contact.cy.ts
@@ -0,0 +1,14 @@
+describe('contact form', () => {
+  it('should submit the form', () => {
+    cy.visit('http://localhost:4200/about');
+    cy.get('[data-cy="contact-input-message"').type('Hello World');
+    cy.get('[data-cy="contact-input-name"]').type('John Doe');
+    cy.get('[data-cy="contact-input-email"]').type('test@example.com');
+    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+    cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
+    cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+
+  })
+})
