ecf2f52c31b1d435941f9889b49806f90c31d6db
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-02 16:42:07 +0300

    Add new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {ElementListenerDirective} from './element-listener.directive';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ElementListenerDirective
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'cypress-e2e-udemy';
+  public getLocBtn = document.getElementById('get-location');
+  public form = document.querySelector('form');
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    $event.returnValue = false;
+  }
+
+
 }
+
