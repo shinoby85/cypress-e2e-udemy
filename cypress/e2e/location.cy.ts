889a38209599a504a96b547b1aeeade91c6db971
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-03 16:27:42 +0300

    Use subs with promise

--- cypress/e2e/location.cy.ts
+++ cypress/e2e/location.cy.ts
@@ -1,5 +1,5 @@
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then(win => {
       cy.stub(win.navigator.geolocation, 'getCurrentPosition')
         .as('getUserPosition')
@@ -13,9 +13,23 @@ describe('share location', () => {
             });
           }, 100);
         });
+      cy.stub(win.navigator.clipboard, 'writeText')
+        .as('saveToClipboard')
+        .resolves();
     });
+  })
+  it('should fetch the user location', () => {
+
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+  });
+
+  it('should share a location URL', () => {
+    cy.get('[data-cy="name-input"]').type('John Doe');
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get('@saveToClipboard').should('have.been.called');
+
   });
 });
