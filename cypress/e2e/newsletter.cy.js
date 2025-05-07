3013b9f5588f3532d9be6f5bbbdee71dc2aa13d5
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-07 10:59:43 +0300

    Use intercept() function

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -4,9 +4,21 @@
    });
 
    it('should display a success message', () => {
+     cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe');
      cy.visit('/');
      cy.get('[data-cy="newsletter-email"]').type('test@example.com');
      cy.get('[data-cy="newsletter-submit"]').click();
+     cy.wait('@subscribe');
      cy.contains('Thanks for signing up!');
+   });
+
+   it('should display validation errors', () => {
+     const httpErrorMessage = 'Email exists already.'
+     cy.intercept('POST', '/newsletter*', { message: httpErrorMessage }).as('subscribe');
+     cy.visit('/');
+     cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+     cy.get('[data-cy="newsletter-submit"]').click();
+     cy.wait('@subscribe');
+     cy.contains(httpErrorMessage);
    });
  })
