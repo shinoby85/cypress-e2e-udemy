7ecbcb512332f0e1584dab9d6361638fd457ed0a
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-07 11:41:37 +0300

    Add click() method to fix test

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -6,6 +6,7 @@
    it('should display a success message', () => {
      cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe');
      cy.visit('/');
+     cy.get('[data-cy="newsletter-email"]').click();
      cy.get('[data-cy="newsletter-email"]').type('test@example.com');
      cy.get('[data-cy="newsletter-submit"]').click();
      cy.wait('@subscribe');
